4e3de675159653dd2255f5e285ce6fabfae1158b
Gaoridang <oingc@naver.com>
2023-09-30 00:59:36 +0900

    Design: 에디터 메뉴 디자인 수정

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -7,34 +7,40 @@ import { Editor, EditorContent, useEditor } from '@tiptap/react';
 import Underline from '@tiptap/extension-underline';
 import StarterKit from '@tiptap/starter-kit';
 
-import { PiTextItalicLight, PiTextStrikethroughLight } from 'react-icons/pi';
+import { PiTextItalicLight, PiTextStrikethroughLight, PiTextUnderlineLight } from 'react-icons/pi';
 import { BsCode } from 'react-icons/bs';
-import {
-  AiOutlineUnorderedList,
-  AiOutlineOrderedList,
-  AiOutlineBold,
-  AiOutlineUnderline,
-} from 'react-icons/ai';
+import { AiOutlineUnorderedList, AiOutlineOrderedList, AiOutlineBold } from 'react-icons/ai';
+import { BiSolidQuoteAltLeft } from 'react-icons/bi';
+import { MdHorizontalRule, MdFormatColorText } from 'react-icons/md';
+import { useState } from 'react';
 
 interface Props {
   editor: Editor;
 }
 
 const MenuBar = ({ editor }: Props) => {
-  // const { editor } = useCurrentEditor();
+  const [menuOpen, setMenuOpen] = useState<string | null>(null);
+
+  const handleMenuSelect = (selectedMenu: string) => {
+    if (menuOpen === selectedMenu) {
+      setMenuOpen(null);
+    } else {
+      setMenuOpen(selectedMenu);
+    }
+  };
 
   if (!editor) {
     return null;
   }
 
   return (
-    <div className="menu">
+    <div className="menus">
       <button
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
         className={editor.isActive('bold') ? 'is-active' : ''}
       >
-        <AiOutlineBold />
+        <AiOutlineBold size={19} />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleItalic().run()}
@@ -48,7 +54,7 @@ const MenuBar = ({ editor }: Props) => {
         disabled={!editor.can().chain().focus().toggleUnderline().run()}
         className={editor.isActive('underline') ? 'is-active' : ''}
       >
-        <AiOutlineUnderline />
+        <PiTextUnderlineLight />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleStrike().run()}
@@ -57,7 +63,8 @@ const MenuBar = ({ editor }: Props) => {
       >
         <PiTextStrikethroughLight />
       </button>
-      <button
+
+      {/* <button
         onClick={() => editor.chain().focus().setParagraph().run()}
         className={editor.isActive('paragraph') ? 'is-active' : ''}
       >
@@ -68,19 +75,28 @@ const MenuBar = ({ editor }: Props) => {
         className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
       >
         소제목
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        <AiOutlineUnorderedList />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        <AiOutlineOrderedList />
-      </button>
+      </button> */}
+
+      <div className="editor-menu-items-wrapper" onClick={() => handleMenuSelect('listMenu')}>
+        <button>
+          <AiOutlineUnorderedList />
+        </button>
+        <div className={`editor-menu-drop ${menuOpen === 'listMenu' ? 'open' : ''}`}>
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive('bulletList') ? 'is-active' : ''}
+          >
+            <AiOutlineUnorderedList />
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            className={editor.isActive('orderedList') ? 'is-active' : ''}
+          >
+            <AiOutlineOrderedList />
+          </button>
+        </div>
+      </div>
+
       <button
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
         className={editor.isActive('codeBlock') ? 'is-active' : ''}
@@ -91,21 +107,32 @@ const MenuBar = ({ editor }: Props) => {
         onClick={() => editor.chain().focus().toggleBlockquote().run()}
         className={editor.isActive('blockquote') ? 'is-active' : ''}
       >
-        인용구
+        <BiSolidQuoteAltLeft />
       </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>구분선</button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-        className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
-      >
-        purple
+      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        <MdHorizontalRule />
       </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#0d0d0d').run()}
-        className={editor.isActive('textStyle', { color: '#0d0d0d' }) ? 'is-active' : ''}
-      >
-        black
-      </button>
+
+      {/* TEXT COLORS */}
+      <div className="editor-menu-items-wrapper" onClick={() => handleMenuSelect('colorMenu')}>
+        <button>
+          <MdFormatColorText />
+        </button>
+        <div className={`editor-menu-drop ${menuOpen === 'colorMenu' ? 'open' : ''}`}>
+          <button
+            onClick={() => editor.chain().focus().setColor('#fc4c70').run()}
+            className={`editor-text-color color-accent ${
+              editor.isActive('textStyle', { color: '#fc4c70' }) ? 'is-active' : ''
+            }`}
+          ></button>
+          <button
+            onClick={() => editor.chain().focus().setColor('#0d0d0d').run()}
+            className={`editor-text-color color-black ${
+              editor.isActive('textStyle', { color: '#0d0d0d' }) ? 'is-active' : ''
+            }`}
+          ></button>
+        </div>
+      </div>
     </div>
   );
 };
