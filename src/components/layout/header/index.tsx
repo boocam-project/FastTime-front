189aa603f00a17b782dfe3ebee29673475d1db21
IAMISTP <hyeminpak@gmail.com>
2023-10-05 17:23:05 +0900

    Feat: 헤더 작업 완료

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,10 +1,33 @@
+import { Link } from 'react-router-dom';
+import styles from './header.module.scss';
 import ToggleBar from '../toggleBar';
-
 const Header = () => {
   return (
     <>
       <ToggleBar />
-      <div></div>
+      <div className={styles.container}>
+        <div className={styles.logo}>fast time</div>
+        <div className={styles.nav}>
+          <div>
+            <Link to={'/'}>소근소근</Link>
+          </div>
+          <div>
+            <Link to={'/'}>포트폴리오</Link>
+          </div>
+          <div>
+            <Link to={'/'}>스터디</Link>
+          </div>
+          <div>
+            <Link to={'/'}>프로젝트</Link>
+          </div>
+          <div>
+            <Link to={'/'}>구인구직</Link>
+          </div>
+        </div>
+        <div className={styles.signin}>
+          <Link to={'/signin'}>signin</Link>
+        </div>
+      </div>
     </>
   );
 };
