91ff53dd4b8f0d401850167741d2fd628d8a7a7f
Gaoridang <oingc@naver.com>
2023-10-06 15:59:28 +0900

    Feat: 게시판 글 리스트 페이지

---
+++ src/components/community/articles.ts
@@ -0,0 +1,84 @@
+export interface Article {
+  id: number;
+  user: string;
+  title: string;
+  description: string;
+  likes: number;
+  createdAt: string;
+}
+
+export const articles: Article[] = [
+  {
+    id: 1,
+    user: 'John Doe',
+    title:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur asfa adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 10,
+  },
+  {
+    id: 2,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+  {
+    id: 3,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+  {
+    id: 4,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+  {
+    id: 5,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+  {
+    id: 6,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+  {
+    id: 7,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+  {
+    id: 8,
+    user: 'John Doe',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros euismod, tincidunt diam ac, tincidun',
+    createdAt: '2021-01-01',
+    likes: 9,
+  },
+];
