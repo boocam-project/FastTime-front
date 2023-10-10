5fb01930e23f05f4e2969ae66c81f3514c4794fa
IAMISTP <hyeminpak@gmail.com>
2023-10-10 20:41:50 +0900

    Feat: 경로 추가 및 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import SignIn from '@/components/signIn';
+import Mypage from '@/pages/myPage';
 
 export const router = createBrowserRouter([
   {
@@ -15,10 +16,7 @@ export const router = createBrowserRouter([
         path: '/signin',
         element: <SignIn />,
       },
-      {
-        path: '/signup',
-        element: <h1>SignUp</h1>,
-      },
+      { path: '/mypage', element: <Mypage /> },
     ],
   },
 ]);
