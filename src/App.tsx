4b8eec1b399e4ef8dbd64999ebda81048a397f00
moonyah <dltk456@naver.com>
2023-10-10 16:19:49 +0900

    signUp파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 import Footer from './components/layout/footer';
 import Header from './components/layout/header';
+import SignIn from './components/signIn';
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <SignIn />
       <Footer />
     </>
   );
