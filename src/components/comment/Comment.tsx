9ef11c187e01a3290dd4f83c8815b300d54dde9a
Gaoridang <oingc@naver.com>
2023-10-12 18:10:21 +0900

    Feat: 게시글 및 댓글 불러오기

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,22 +1,45 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './index.module.scss';
 import Button from '../atoms/button';
 import CommentList from './CommentList';
+import { instance } from '@/api/client';
 
-const Comment = () => {
-  const [content, setContent] = useState();
+interface Props {
+  postId: number;
+}
+
+const Comment = ({ postId }: Props) => {
+  const [content, setContent] = useState('');
+  const [anonymity, setAnonymity] = useState<boolean>(false);
 
   const handleComment = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(content);
+    setContent(e.currentTarget.value);
 
     e.currentTarget.style.height = 'inherit';
     e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
   };
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = {
+      postId,
+      memberId: 1,
+      content,
+      anonymity,
+      parentCommentId: null,
+    };
+
+    console.log(data);
+
+    const response = await instance.post('api/v1/comment', data);
+    console.log(response.status);
+  };
+
   return (
     <div className={styles['comment-box']}>
       <h2 className={styles.title}>댓글</h2>
-      <form className={styles.form} action="">
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles['content-write']}>
           <textarea
             // rows={1}
@@ -25,9 +48,13 @@ const Comment = () => {
             onChange={handleComment}
             placeholder="댓글을 입력하세요."
           />
-          <Button className="default-red-200" type="submit" show>
-            등록
-          </Button>
+          <div className={styles.buttons}>
+            <input type="checkbox" id="anonymity" onChange={() => setAnonymity((prev) => !prev)} />
+            <label htmlFor="anonymity">익명</label>
+            <Button type="submit" className="default-red-200" show>
+              등록
+            </Button>
+          </div>
         </div>
       </form>
       {/* 댓글 목록 */}
