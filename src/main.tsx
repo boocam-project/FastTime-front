7313fe7f7fe7760ae770107ca7655072f467a567
IAMISTP <hyeminpak@gmail.com>
2023-10-16 02:10:19 +0900

    recoil 사용자 정보 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router.tsx';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, atom } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { recoilPersist } from 'recoil-persist';
+
+type UserStateType = {
+  id: number;
+  nickname: string;
+};
+const { persistAtom } = recoilPersist();
+
+export const userState = atom<UserStateType>({
+  key: 'userState',
+  default: {
+    id: 0,
+    nickname: '',
+  },
+  effects_UNSTABLE: [persistAtom],
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-      </RecoilRoot>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
