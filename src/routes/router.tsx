58642c04bfc4159f66d7f9be14a189885d4f0a3c
gaori <monooi@naver.com>
2023-10-17 20:07:15 +0900

    Feat: 게시글 신고, 수정, 익명 댓글

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,6 +7,7 @@ import TextEditor from '@/components/editor';
 import SignUpForm from '@/components/signUp/SignUpForm';
 import Mypage from '@/pages/myPage';
 import ArticleList from '@/components/community/ArticleList';
+import EditArticle from '@/components/editor/EditArticle';
 
 export const router = createBrowserRouter([
   {
@@ -39,5 +40,9 @@ export const router = createBrowserRouter([
   {
     path: '/write',
     element: <TextEditor />,
+  },
+  {
+    path: '/edit/:id',
+    element: <EditArticle />,
   },
 ]);
