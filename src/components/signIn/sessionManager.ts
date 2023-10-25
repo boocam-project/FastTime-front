63d2359293bc8e2ede38987362f2eddb8278d9ec
moonyah <dltk456@naver.com>
2023-10-25 19:14:38 +0900

    Feat: sessionManager.ts로 세션 관리

---
+++ src/components/signIn/sessionManager.ts
@@ -0,0 +1,41 @@
+// import { useCallback, useEffect } from 'react';
+// import { useRecoilState } from 'recoil';
+// import { userState } from '@/store/store';
+// import { useNavigate } from 'react-router-dom';
+
+// const SessionExpirationCheck = () => {
+//   const [userData, setUserData] = useRecoilState(userState);
+//   const navigation = useNavigate();
+
+//   const checkSessionStatus = useCallback(async () => {
+//     try {
+//       const response = await fetch('/api/checkSessionStatus');
+//       if (response.status === 401) {
+//         setUserData({ ...userData, login: false });
+//         navigation('/signin');
+//       } else if (response.status === 200) {
+//         // 세션이 유효한 경우
+//         const userData = await response.json();
+//         setUserData(userData);
+//       }
+//     } catch (error) {
+//       console.error('세션 확인 중 오류 발생', error);
+//     }
+//   }, [userData, setUserData, navigation]);
+
+//   useEffect(() => {
+//     checkSessionStatus();
+
+//     const interval = setInterval(
+//       () => {
+//         checkSessionStatus();
+//       },
+//       1000 * 60 * 30
+//     );
+
+//     return () => clearInterval(interval);
+//   }, [checkSessionStatus]);
+//   return null;
+// };
+
+// export default SessionExpirationCheck;
