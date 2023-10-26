b8d90d3ae4f440efeb617c93d66a080bebca3010
gaori <monooi@naver.com>
2023-10-26 14:59:55 +0900

    Feat: 좋아요 및 좋아요 취소 optimistic update 적용

---
+++ src/hooks/useLikeMutations.ts
@@ -0,0 +1,88 @@
+import { instance } from '@/api/client';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+interface LikeProps {
+  postId: number;
+  isLike: boolean;
+  memberId: number;
+  id: number;
+}
+
+interface LikeResponse {
+  code: number;
+  message: string;
+  data: LikeProps;
+}
+
+const useLikeMutations = () => {
+  const queryClient = useQueryClient();
+
+  const likeMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const response = await instance.post<LikeResponse>('api/v1/record/', {
+        postId: id,
+        isLike: true,
+      });
+
+      return response.data;
+    },
+
+    onMutate: async (postId: number) => {
+      await queryClient.cancelQueries({ queryKey: ['like', postId] });
+      const previousLike = queryClient.getQueryData<LikeResponse>(['like', postId]);
+
+      queryClient.setQueryData(['like', postId], (old: LikeProps) => ({
+        ...old,
+        isLike: true,
+      }));
+
+      return { previousLike };
+    },
+
+    onError: (err, _newLike, context) => {
+      if (context) {
+        queryClient.setQueryData(['like', context.previousLike], context.previousLike);
+      }
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['like'] });
+    },
+  });
+
+  const unlikeMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const response = await instance.delete<LikeResponse>('api/v1/record/', {
+        data: { postId: id },
+      });
+
+      return response.data;
+    },
+
+    onMutate: async (postId: number) => {
+      await queryClient.cancelQueries({ queryKey: ['like', postId] });
+      const previousLike = queryClient.getQueryData<LikeResponse>(['like', postId]);
+
+      queryClient.setQueryData(['like', postId], (old: LikeProps) => ({
+        ...old,
+        isLike: false,
+      }));
+
+      return { previousLike };
+    },
+
+    onError: (err, _newLike, context) => {
+      if (context) {
+        queryClient.setQueryData(['like', context.previousLike], context.previousLike);
+      }
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['like'] });
+    },
+  });
+
+  return { likeMutation, unlikeMutation };
+};
+
+export default useLikeMutations;
