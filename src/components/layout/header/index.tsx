410028bb3102b57b49b80a8ec3981752b5c82d01
gaori <monooi@naver.com>
2023-10-30 19:34:25 +0900

    Design: 헤더 디자인 수정

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,102 +1,18 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import styles from './header.module.scss';
-import { AiOutlineMenu } from 'react-icons/ai';
-import { useRecoilState } from 'recoil';
-import { userState } from '@/store/store';
-import { instance } from '@/api/client';
-import { AiOutlineUser } from 'react-icons/ai';
-import logo from '@/assets/logo.svg';
 import MobileMenu from './MobileMenu';
-
-const fetchLogout = async () => {
-  const response = await instance.get(`api/v1/logout`);
-  return response.data;
-};
+import Header2 from './Header2';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [userData, setUserData] = useRecoilState(userState);
 
-  const navigation = useNavigate();
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
   const handleMenuClose = () => {
     setIsMenuOpen(false); // "닫기" 버튼 클릭 시 메뉴를 닫음
-  };
-
-  const showAlert = () => {
-    window.alert('준비 중입니다');
-  };
-
-  const handleLogoClick = () => {
-    navigation('/community');
-  };
-
-  const logoutHandler = () => {
-    const fetchData = async () => {
-      const result = await fetchLogout();
-      if (result.code === 200) {
-        setUserData({ ...userData, login: false });
-        if (userData.nickname === '관리자') {
-          navigation('/admin/login');
-        } else {
-          navigation('/signin');
-        }
-      }
-    };
-    fetchData();
   };
 
   return (
     <>
       {isMenuOpen && <MobileMenu onClose={handleMenuClose} />}
-      <div className={styles.container}>
-        <div className={styles['menu-btn']} onClick={toggleMenu}>
-          <AiOutlineMenu />
-        </div>
-        <div className={styles.logo} onClick={handleLogoClick}>
-          <img src={logo} alt="Logo" />
-        </div>
-        <div className={styles.nav}>
-          <div>
-            <Link to={'/community'} className={styles['nav-link']}>
-              소근소근
-            </Link>
-          </div>
-          <div>
-            <Link to={'/'} onClick={showAlert}>
-              포트폴리오
-            </Link>
-          </div>
-          <div>
-            <Link to={'/'} onClick={showAlert}>
-              스터디
-            </Link>
-          </div>
-          <div>
-            <Link to={'/'} onClick={showAlert}>
-              프로젝트
-            </Link>
-          </div>
-        </div>
-        <div className={styles.signin}>
-          {userData.login ? (
-            <>
-              <button onClick={logoutHandler}>logout</button>
-              {userData.nickname === '관리자' ? (
-                <></>
-              ) : (
-                <AiOutlineUser onClick={() => navigation('/mypage')} />
-              )}
-            </>
-          ) : (
-            <Link to={'/signin'}>signin</Link>
-          )}
-        </div>
-      </div>
+      <Header2 />
     </>
   );
 };
