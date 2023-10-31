adeb95eef071b34cbd90ebae0c343aefc640e702
gaori <monooi@naver.com>
2023-10-31 21:40:14 +0900

    Fix: 오타 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,7 +5,7 @@ import SignIn from '@/components/signIn';
 import ArticleDetail from '@/components/community/ArticleDetail';
 import TextEditor from '@/components/editor';
 import SignUp from '@/components/signUp';
-import Mypage from '@/pages/myPage';
+import MyPage from '@/pages/myPage';
 import ArticleList from '@/components/community/ArticleList';
 import EditArticle from '@/components/editor/EditArticle';
 import AdminLogin from '@/pages/adminLogin';
@@ -37,7 +37,7 @@ export const router = createBrowserRouter([
         path: '/community/:id',
         element: <ArticleDetail />,
       },
-      { path: '/mypage', element: <Mypage /> },
+      { path: '/mypage', element: <MyPage /> },
       { path: '/admin/login', element: <AdminLogin /> },
       { path: '/admin/board', element: <AdminBoard /> },
       { path: '/admin/detail/:id', element: <AdminBoardDetail /> },
