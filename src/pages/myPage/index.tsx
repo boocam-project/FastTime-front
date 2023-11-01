3e525e76fb13b3494af71412e32034a512b0f71e
gaori <monooi@naver.com>
2023-11-01 21:20:55 +0900

    Design: 설정 페이지 모달 디자인 변경

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -2,11 +2,12 @@ import Input from '@/components/atoms/input';
 import { userState } from '@/store/store';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import SettingsModal from './Modal';
+import SettingsModal from './SettingsModal';
 import styles from './index.module.scss';
 
 const MyPage = () => {
   const user = useRecoilValue(userState);
+  const [nickname, setNickname] = useState(user.nickname);
 
   // if (!user.isLogin) {
   //   alert('로그인이 필요합니다.');
@@ -47,14 +48,26 @@ const MyPage = () => {
           <span>********</span>
         </button>
       </div>
-      <SettingsModal isOpen={modal.isOpen && modal.type === 'change-nickname'} onClose={closeModal}>
-        <h2>닉네임 변경</h2>
-        <Input name="name" label="닉네임" value="" />
+      <SettingsModal
+        title="닉네임 변경"
+        isOpen={modal.isOpen && modal.type === 'change-nickname'}
+        onClose={closeModal}
+      >
+        <Input name="name" label="닉네임" value={nickname} />
       </SettingsModal>
-      <SettingsModal isOpen={modal.isOpen && modal.type === 'reset-password'} onClose={closeModal}>
-        <h2>비밀번호 재설정</h2>
-        <Input name="password" label="비밀번호" value="" />
-        <Input name="password" label="비밀번호 확인" value="" />
+      <SettingsModal
+        title="비밀번호 재설정"
+        isOpen={modal.isOpen && modal.type === 'reset-password'}
+        onClose={closeModal}
+      >
+        <Input type="password" name="password" label="비밀번호" value="" placeholder="********" />
+        <Input
+          type="password"
+          name="confirm-password"
+          label="비밀번호 확인"
+          placeholder="********"
+          value=""
+        />
       </SettingsModal>
     </div>
   );
