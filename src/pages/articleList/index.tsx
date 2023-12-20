ab304cec6b2666dddbc74e392d8b2e7911c4a241
Gaoridang <oingc@naver.com>
2023-12-20 17:40:05 +0900

    Refactor: 게시글 목록 페이지 컴포넌트 분리
    제목, 메뉴, 게시글 목록 세 부분으로 분리

--- src/pages/articleList/index.tsx
+++ src/pages/articleList/index.tsx
@@ -1,24 +1,18 @@
 import styles from './index.module.scss';
-import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
-import { BsPencilSquare } from 'react-icons/bs';
 import ArticleList from './components/ArticleList';
 import ArticleSkeletons from './components/Skeleton';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './components/ErrorFallback';
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import Descriptions from './components/Descriptions';
+import ArticleMenus from './components/ArticleMenus';
 
 const ArticleListPage = () => {
   return (
-    <>
-      <div className={styles.articleMenus}>
-        <h2 style={{ fontWeight: 'normal' }}>게시글</h2>
-        <Link to={'/write'}>
-          <BsPencilSquare size={20} />
-          <span>글쓰기</span>
-        </Link>
-      </div>
-      {/* TODO: 에러 바운더리 추가 */}
+    <div className={styles.container}>
+      <Descriptions />
+      <ArticleMenus />
       <QueryErrorResetBoundary>
         {({ reset }) => (
           <ErrorBoundary onReset={reset} fallbackRender={ErrorFallback}>
@@ -28,7 +22,7 @@ const ArticleListPage = () => {
           </ErrorBoundary>
         )}
       </QueryErrorResetBoundary>
-    </>
+    </div>
   );
 };
 
