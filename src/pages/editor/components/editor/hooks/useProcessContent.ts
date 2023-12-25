114e9d061be8b8c2265a5b985cc0c598730945a3
gaori <monooi@naver.com>
2023-12-25 17:35:05 +0900

    Refactor: 이미지 처리 과정 순서 변경
    발행 버튼 클릭 시 -> 이미지 추가 시 파이어베이스 주소로 변경

--- src/pages/editor/components/editor/hooks/useProcessContent.ts
+++ src/pages/editor/components/editor/hooks/useProcessContent.ts
@@ -1,4 +1,3 @@
-import { uploadImageToFirebase } from '@/hooks/useImageConvert';
 import { useRecoilValue } from 'recoil';
 import { editorState } from '../atoms/editor.atom';
 
@@ -10,25 +9,6 @@ const useProcessContent = () => {
 
     const parser = new DOMParser();
     const document = parser.parseFromString(content, 'text/html');
-
-    // TODO: 에러 핸들링 과정 추가
-    const images = document.querySelectorAll('img');
-    const imageUploads = Array.from(images).map(async (img, index) => {
-      console.log(`Processing image ${index}`, img.src);
-
-      try {
-        if (img.src.startsWith('https://')) return;
-        const blob = await fetch(img.src).then((res) => res.blob());
-        const firebaseUrl = await uploadImageToFirebase(blob);
-        console.log('Image uploaded. Firebase URL: ', firebaseUrl);
-        img.src = firebaseUrl;
-      } catch (error) {
-        console.error('Error Uploading image:', img.src, error);
-      }
-    });
-
-    await Promise.all(imageUploads);
-    console.log('All image uploads completed');
 
     const titleEl = document.querySelector('h1');
 
--- src/pages/editor/components/editor/index.tsx
+++ src/pages/editor/components/editor/index.tsx
@@ -2,10 +2,10 @@ import './styles.scss';
 import MenuBar from '@/pages/editor/components/editor/MenuBar';
 import { useEditor, EditorContent } from '@tiptap/react';
 import { extensions } from './extensions';
-import useBlobUrl from '@/hooks/useBlobUrl';
 import { Article } from '@/pages/articleDetail/types';
 import { useSetRecoilState } from 'recoil';
 import { editorState } from './atoms/editor.atom';
+import { getFirebaseUrlFromImage } from '../../utils/imageConvert';
 
 interface DefaultEditorProps {
   article?: Article;
@@ -13,7 +13,6 @@ interface DefaultEditorProps {
 
 const DefaultEditor = ({ article }: DefaultEditorProps) => {
   const setClientEditor = useSetRecoilState(editorState);
-  const { createBlobUrl } = useBlobUrl();
   const content = article ? `<h1>${article.title}</h1> ${article.content}` : `<p></p>`;
   const editor = useEditor({
     extensions,
@@ -28,19 +27,22 @@ const DefaultEditor = ({ article }: DefaultEditorProps) => {
     },
     editorProps: {
       handleDrop: (view, event, _slice, moved) => {
-        if (!moved && event.dataTransfer?.files.length) {
-          const file = event.dataTransfer.files[0];
+        const imageDrop = async (file: File) => {
           const fileSize = Number((file.size / 1024 / 1024).toFixed(4)); // MB
           if ((file.type === 'image/jpeg' || file.type === 'image/png') && fileSize < 10) {
-            const blobUrl = createBlobUrl(file);
+            const url = await getFirebaseUrlFromImage(file);
 
             const { tr } = view.state;
-            const imageNode = view.state.schema.nodes.image.create({ src: blobUrl });
+            const imageNode = view.state.schema.nodes.image.create({ src: url });
             const transaction = tr.replaceSelectionWith(imageNode);
             view.dispatch(transaction);
           } else {
             alert('이미지는 jpeg, png 형식만 가능합니다. (10MB 이하)');
           }
+        };
+        if (!moved && event.dataTransfer?.files.length) {
+          const file = event.dataTransfer.files[0];
+          imageDrop(file);
           return true;
         }
         return false;
