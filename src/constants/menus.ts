5dd5f84413df93114907b19d9f6f79154d9eef3d
Lee JaeJun <monooi@naver.com>
2024-02-01 23:51:19 +0900

    Add: 메뉴 리스트 객체 생성

---
+++ src/constants/menus.ts
@@ -0,0 +1,6 @@
+export const MENU_ITEMS = [
+  { name: '소근소근', path: 'community' },
+  { name: '스터디', path: 'study' },
+  { name: '프로젝트', path: 'project' },
+  { name: '포트폴리오', path: 'portfolio' },
+];
