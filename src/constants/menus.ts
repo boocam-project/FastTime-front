8bded3f5636da7f918e7bec4fcd550d97f7af025
2YH02 <yonghuni484@gmail.com>
2024-04-06 00:01:43 +0900

    Design: 활동 리스트 UI 추가

--- src/constants/menus.ts
+++ src/constants/menus.ts
@@ -1,6 +1,7 @@
 export const MENU_ITEMS = [
   { name: '소근소근', path: 'community' },
   { name: '스터디', path: 'study' },
+  { name: '활동', path: 'activity' },
   { name: '프로젝트', path: 'project' },
   { name: '포트폴리오', path: 'portfolio' },
   { name: '리뷰', path: 'review' },
