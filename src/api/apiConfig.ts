7ac91d302e01472e08614ea16d9f470285a60f70
Gaoridang <oingc@naver.com>
2024-07-31 20:54:59 +0900

    Refactor: update login api version

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,7 +2,7 @@ export const API_VERSION = '/api/v2';
 
 export const ENDPOINTS = {
   articles: `${API_VERSION}/articles`,
-  login: import.meta.env.DEV ? `${API_VERSION}/login` : `/api/v1/login`,
+  login: `${API_VERSION}/login`,
   refreshToken: `${API_VERSION}/refresh`,
   members: `/api/v1/members`,
   comments: `/api/v1/comments`,
